9fbd4818187cc5b0c268ced06b6638c30e4d92e8
Davide <davide.miotti@gmail.com>
2026-01-03 13:47:38 +0100

    Better derivative 4 BC

--- src/boundary_conditions/numerical/derivatives.jl
+++ src/boundary_conditions/numerical/derivatives.jl
@@ -1,4 +1,13 @@
 """
+Compute interpolation weights at a point, returning dense vector.
+"""
+@inline function interpolation_weights(nbs_coords, pt; kwargs...)
+    op = regrid(nbs_coords, [pt]; kwargs...)
+    update_weights!(op)
+    return collect(op.weights[1, :])
+end
+
+"""
 Compute derivative weights for a boundary point using standard directional derivative.
 No shadow points - uses direct finite difference.
 Returns (neighbor_indices, weights).
@@ -7,7 +16,7 @@ function compute_local_derivative_weights(
         surf, domain, shadow_op::Nothing, A, global_i, local_i, normals; kwargs...)
 
     # Get neighbors from A (assuming structural symmetry)
-    nbs = view(A.rowval, A.colptr[global_i]:(A.colptr[global_i + 1] - 1))
+    nbs = A.rowval[A.colptr[global_i]:(A.colptr[global_i + 1] - 1)]
 
     # Get coords
     surf_pt = get_node_coords(surf, local_i)
@@ -17,8 +26,9 @@ function compute_local_derivative_weights(
     n = ustrip(normals[local_i])
     d = directional(nbs_coords, [surf_pt], n)
 
-    # d.weights is (1, length(nbs))
-    return Vector(nbs), Vector(d.weights[1, :])
+    # Collect to dense to preserve precision
+    w = collect(d.weights[1, :])
+    return nbs, w
 end
 
 """
@@ -29,7 +39,7 @@ Returns (neighbor_indices, weights).
 function compute_local_derivative_weights(
         surf, domain, shadow_op::WhatsThePoint.ShadowPoints{1},
         A, global_i, local_i, normals; kwargs...)
-    nbs = view(A.rowval, A.colptr[global_i]:(A.colptr[global_i + 1] - 1))
+    nbs = A.rowval[A.colptr[global_i]:(A.colptr[global_i + 1] - 1)]
 
     surf_pt = get_node_coords(surf, local_i)
     nbs_coords = [get_node_coords(domain.cloud, nb) for nb in nbs]
@@ -37,23 +47,12 @@ function compute_local_derivative_weights(
     n = ustrip(normals[local_i])
     d = ustrip(shadow_op.Δ(surf_pt))
 
-    # Shadow point
+    # Compute derivative using shadow point: (u_surf - u_shadow)/Δ
     shadow_pt = surf_pt .- n .* d
-
-    # Interpolation weights for surface point (regrid)
-    op_surf = regrid(nbs_coords, [surf_pt]; kwargs...)
-    update_weights!(op_surf)
-    w_surf = op_surf.weights[1, :]
-
-    # Interpolation weights for shadow point
-    op_shadow = regrid(nbs_coords, [shadow_pt]; kwargs...)
-    update_weights!(op_shadow)
-    w_shadow = op_shadow.weights[1, :]
-
-    # Derivative weights: (w_surf - w_shadow) / d
-    w_deriv = (w_surf .- w_shadow) ./ d
-
-    return Vector(nbs), Vector(w_deriv)
+    w_surf = interpolation_weights(nbs_coords, surf_pt; kwargs...)
+    w_shadow = interpolation_weights(nbs_coords, shadow_pt; kwargs...)
+    w_deriv = @. (w_surf - w_shadow) / d
+    return nbs, w_deriv
 end
 
 """
@@ -64,7 +63,7 @@ Returns (neighbor_indices, weights).
 function compute_local_derivative_weights(
         surf, domain, shadow_op::WhatsThePoint.ShadowPoints{2},
         A, global_i, local_i, normals; kwargs...)
-    nbs = view(A.rowval, A.colptr[global_i]:(A.colptr[global_i + 1] - 1))
+    nbs = A.rowval[A.colptr[global_i]:(A.colptr[global_i + 1] - 1)]
 
     surf_pt = get_node_coords(surf, local_i)
     nbs_coords = [get_node_coords(domain.cloud, nb) for nb in nbs]
@@ -72,23 +71,12 @@ function compute_local_derivative_weights(
     n = ustrip(normals[local_i])
     d = ustrip(shadow_op.Δ(surf_pt))
 
+    # Compute derivative using 2nd order shadow points
     shadow_pt1 = surf_pt .- n .* d
     shadow_pt2 = surf_pt .- n .* (2 * d)
-
-    op_surf = regrid(nbs_coords, [surf_pt]; kwargs...)
-    update_weights!(op_surf)
-    w_surf = op_surf.weights[1, :]
-
-    op_shadow1 = regrid(nbs_coords, [shadow_pt1]; kwargs...)
-    update_weights!(op_shadow1)
-    w_shadow1 = op_shadow1.weights[1, :]
-
-    op_shadow2 = regrid(nbs_coords, [shadow_pt2]; kwargs...)
-    update_weights!(op_shadow2)
-    w_shadow2 = op_shadow2.weights[1, :]
-
-    # (3*u_s - 4*u_sh1 + u_sh2) / 2d
-    w_deriv = (3 .* w_surf .- 4 .* w_shadow1 .+ w_shadow2) ./ (2 * d)
-
-    return Vector(nbs), Vector(w_deriv)
+    w_surf = interpolation_weights(nbs_coords, surf_pt; kwargs...)
+    w_shadow1 = interpolation_weights(nbs_coords, shadow_pt1; kwargs...)
+    w_shadow2 = interpolation_weights(nbs_coords, shadow_pt2; kwargs...)
+    w_deriv = @. (3 * w_surf - 4 * w_shadow1 + w_shadow2) / (2 * d)
+    return nbs, w_deriv
 end
